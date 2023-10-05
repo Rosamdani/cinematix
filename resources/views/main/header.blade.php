b8a328a3730797a260df23e21e0b36b3e7ad1c27
Rosamdani <rosyamdani2000018114@webmail.uad.ac.id>
2023-10-06 01:06:54 +0700

    first commit

---
+++ resources/views/main/header.blade.php
@@ -0,0 +1,8 @@
+<meta charset="utf-8" />
+<title>Project Management</title>
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta content="A fully featured admin theme which can be used to build CRM, CMS, etc." name="description" />
+<meta content="Coderthemes" name="author" />
+<meta http-equiv="X-UA-Compatible" content="IE=edge" />
+<script src="https://cdn.tailwindcss.com"></script>
+<script src="https://code.jquery.com/jquery-3.7.1.min.js"></script>
---
+++ resources/views/main/index.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    @include('main.header')
+</head>
+
+<body class="loading" data-layout-color="light" data-layout-mode="default" data-layout-size="fluid"
+    data-topbar-color="light" data-leftbar-position="fixed" data-leftbar-color="light" data-leftbar-size='default'
+    data-sidebar-user='true'>
+
+    <!-- Begin page -->
+    <div id="wrapper">
+        @include('main.navbar')
+        @yield('content')
+        @include('main.footer')
+    {{-- tutup div wrapper ada di file footer.blade.php --}}
+    </div>
+    @include('main.script')
+</body>
+
+
+</html>
